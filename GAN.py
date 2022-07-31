7f8a5bacb0306d8105d41408e0c5f3791e2299d4
TensorNd <do-not-mail@nothing.com>
2022-07-31 21:41:53 +0530

    Added the Generator and Discriminator instances to device, Rectified the datatype of tensors and ensured proper shape gets passed into the model code.

--- GAN.py
+++ GAN.py
@@ -1,26 +1,32 @@
 import model
+from numpy import array
 
 class NLP_GAN:
     def __init__(self, input_vocab_size, hidden_dim, output_vocab_size, max_sequence_length, n_classes, SOS, EOS):
-        self.Generator = model.Generator(input_vocab_size, hidden_dim, output_vocab_size, max_sequence_length, SOS, EOS)
-        self.Discriminator = model.Discriminator(output_vocab_size, hidden_dim, n_classes)
+        self.Generator = model.Generator(input_vocab_size, hidden_dim, output_vocab_size, max_sequence_length, SOS, EOS).to(model.device)
+        self.Discriminator = model.Discriminator(output_vocab_size, hidden_dim, n_classes).to(model.device)
         self.k = 1
     def train(self, X, Y, epochs):
-        for epoch in epochs:
-            labels = []
-            inputs = []
+        X = model.torch.tensor(X).type(model.torch.LongTensor).to(model.device)
+        Y = model.torch.tensor(Y).type(model.torch.LongTensor).to(model.device)
+        for epoch in range(epochs):
+            print(f"EPOCH[{epoch}]")
             for Xi, Yi in zip(X,Y):
-                fake_data = self.Generator(Xi)
+                labels = []
+                inputs = []
+                fake_data = array(self.Generator(Xi)).reshape(-1,1)
                 inputs.append(fake_data)
                 labels.append(1)
-                orignal_data = Yi
+                orignal_data = Yi.cpu().numpy()
                 inputs.append(orignal_data)
                 labels.append(0)
-            inputs = torch.tensor(inputs)
-            labels = torch.tensor(labels)
-            for iteration in range(self.k):
-                self.Discriminator.train(inputs, labels)
-            self.Generator.train(Xi, Yi)
+                inputs = array(inputs)
+                labels = array(labels)
+                inputs = model.torch.tensor(inputs, device=model.device)
+                labels = model.torch.tensor(labels, device=model.device).view(-1,1,1).type(model.torch.FloatTensor)
+                for iteration in range(self.k):
+                    self.Discriminator.train(inputs, labels)
+                self.Generator.train(Xi, Yi)
     def generate(self, X):
         return self.Generator(X)
     
