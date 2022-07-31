bab32e1ae8aba6e349d732851bc4e8e8d4b1ec77
TensorNd <do-not-mail@nothing.com>
2022-07-31 21:43:18 +0530

    Added code for instantiating the GAN model and calling the training code.

--- orchestrator.py
+++ orchestrator.py
@@ -6,9 +6,18 @@ import torch
 if __name__ == '__main__':
     preprocessor = preprocessing.Preprocessing()
     X, Y = preprocessor.get_X_Y()
+    # X = torch.from_numpy(X).type(torch.LongTensor)
+    # Y = torch.from_numpy(Y).type(torch.LongTensor)
+    input_vocab_size, output_vocab_size = preprocessor.getVocabSize()
+    hidden_dim = 128
+    n_classes = 1
+    max_sequence_length = max([i.shape[0] for i in X]) - 1
+    # print(X.size(), Y.size())
+    SOS, EOS = preprocessor.get_SOS_EOS_token()
+    gan = GAN.NLP_GAN(input_vocab_size, hidden_dim, output_vocab_size, max_sequence_length, n_classes, SOS, EOS)
+    gan.train(X, Y, 5)
     # idx = randint(0,X.shape[0]-11)
     # print(X[idx : idx + 10])
     # print(Y[idx : idx + 10])
-    X = torch.tensor(X)
-    print(X)
+    
     
