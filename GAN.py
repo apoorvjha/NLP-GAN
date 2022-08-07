f2f366ac4af8c6847ba5cd2a4b0917a952d6ad7f
TensorNd <do-not-mail@nothing.com>
2022-08-07 20:56:36 +0530

    Added code to save and load the model checkpoints. Fixed the generate function by converting input matrix to tensor.

--- GAN.py
+++ GAN.py
@@ -1,10 +1,15 @@
 import model
 from numpy import array
+from torch import save, load
 
 class NLP_GAN:
-    def __init__(self, input_vocab_size, hidden_dim, output_vocab_size, max_sequence_length, n_classes, SOS, EOS):
-        self.Generator = model.Generator(input_vocab_size, hidden_dim, output_vocab_size, max_sequence_length, SOS, EOS).to(model.device)
-        self.Discriminator = model.Discriminator(output_vocab_size, hidden_dim, n_classes).to(model.device)
+    def __init__(self, input_vocab_size=None, hidden_dim=None, output_vocab_size=None, max_sequence_length=None, n_classes=None, SOS=None, EOS=None):
+        if input_vocab_size == None:
+            self.Generator = load('Generator.pt')
+            self.Discriminator = load('Discriminator.pt')
+        else:
+            self.Generator = model.Generator(input_vocab_size, hidden_dim, output_vocab_size, max_sequence_length, SOS, EOS).to(model.device)
+            self.Discriminator = model.Discriminator(output_vocab_size, hidden_dim, n_classes).to(model.device)
         self.k = 1
     def train(self, X, Y, epochs):
         X = model.torch.tensor(X).type(model.torch.LongTensor).to(model.device)
@@ -27,7 +32,10 @@ class NLP_GAN:
                 for iteration in range(self.k):
                     self.Discriminator.train(inputs, labels)
                 self.Generator.train(Xi, Yi)
+            save(self.Generator, 'Generator.pt')
+            save(self.Discriminator, 'Discriminator.pt')
     def generate(self, X):
+        X = model.torch.tensor(X).type(model.torch.LongTensor).to(model.device)
         return self.Generator(X)
     
 
